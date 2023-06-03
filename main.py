30cd67bb8580970db02e6d4c44f3837c8d9363d8
Oghenekome Edaware <edawarekome@gmail.com>
2023-06-03 14:46:37 +0100

    add subtraction test

--- main.py
+++ main.py
@@ -2,8 +2,8 @@
 def addition(num1, num2):
     return num1 + num2
 
-# def subtraction(num1, num2):
-#     return num1 - num2
+def subtraction(num1, num2):
+    return num1 - num2
 
 # print("Hello world")
 # print("This is an update")
