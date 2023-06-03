d31c311b1f2bbcd443a18478ec0e68aa119adf04
Oghenekome Edaware <edawarekome@gmail.com>
2023-06-03 12:23:34 +0100

    add test for subtraction

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
 
-print("Hello world")
-print("This is an update")
+def addition(num1, num2):
+    return num1 + num2
+
+def subtraction(num1, num2):
+    return num1 - num2
+
+# print("Hello world")
+# print("This is an update")
 
 #extra line
+
