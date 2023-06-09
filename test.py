978ff9e58a6c49b496c1a8e32284dbf0d9e5218d
Oghenekome Edaware <edawarekome@gmail.com>
2023-06-09 15:07:43 +0100

    my addition and subtraction test

--- test.py
+++ test.py
@@ -14,6 +14,7 @@ class TestMain(unittest.TestCase):
         self.assertEqual(main.subtraction(5, 2), 3, "Should be 3")
         self.assertEqual(main.subtraction(7, 2), 5, "Should be 3")
         self.assertEqual(main.subtraction(9, 7), 2, "Should be 3")
+        self.assertEqual(main.subtraction(10, 7), 3, "Should be 3")
 
 if __name__ == "__main__":
     unittest.main()
