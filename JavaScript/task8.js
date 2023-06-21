d3bfabbc33303259ef709af92d8437b17194f915
Oghenekome Edaware <edawarekome@gmail.com>
2023-06-21 22:23:22 +0100

    for-loop

--- JavaScript/task8.js
+++ JavaScript/task8.js
@@ -1,7 +1,10 @@
 // A JavaScript program that uses a for-loop to print out all the odd numbers between 1 and 20
 
 for (let number = 1; number <= 20; number = number + 1) {
+
     if(number % 2 !==0 ) {
+
         console.log(number)
+        
     }
 }
