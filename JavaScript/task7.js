0d3495c19968520825927edb7dae2f47eebf1b60
Oghenekome Edaware <edawarekome@gmail.com>
2023-06-21 13:18:24 +0100

    if else if lesson

---
+++ JavaScript/task7.js
@@ -0,0 +1,16 @@
+const side1 = 14
+const side2 = 14
+const side3 = 14
+
+if (side1 === side2 && side2 === side3) {
+
+    console.log('Equilateral triangle')
+
+} else if (side1 === side2 || side1 === side3 || side2 === side3) {
+
+    console.log('Isosceles triangle')
+
+} else {
+
+    console.log ('Scalene triangle')
+}
