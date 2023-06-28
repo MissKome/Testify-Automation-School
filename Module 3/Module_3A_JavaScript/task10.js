a9317bf03aaf09faad29adc1152a73193b3d6d12
Oghenekome Edaware <edawarekome@gmail.com>
2023-06-28 19:09:00 +0100

    Area of  Rectangle 2

--- Module 3/Module_3A_JavaScript/task10.js
+++ Module 3/Module_3A_JavaScript/task10.js
@@ -1,3 +1,4 @@
+//The function calculateAreaOfRectangle takes two parameters length and width, multiplies them, and returns the area of the rectangle.
 
 function calculateAreaOfRectangle(length, width) {
     const rectangleArea = length * width;
