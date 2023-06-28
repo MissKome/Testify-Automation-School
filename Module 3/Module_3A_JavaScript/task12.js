72099379a0699b0103e70d8d93a38a689a03fd7d
Oghenekome Edaware <edawarekome@gmail.com>
2023-06-28 21:01:48 +0100

    Pick your Best Friend

---
+++ Module 3/Module_3A_JavaScript/task12.js
@@ -0,0 +1,7 @@
+//choose your best friend using their index in the array
+
+const friends = ['John', 'Doe', 'Mary', 'Jane', 'Nik'];
+
+const bestFriend =  friends[2]; // Index 2 corresponds to the third friend, "Mary"
+
+console.log(bestFriend);
