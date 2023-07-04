3d426d94f3f5a415cccbe0c6a2473ab29d8c1b54
Oghenekome Edaware <edawarekome@gmail.com>
2023-07-04 14:17:39 +0100

    My Personal Library

---
+++ Module 3/Module_3A_JavaScript/task13.js
@@ -0,0 +1,16 @@
+//A JavaScript object called books using object literal notation
+
+const books = {
+    title: 'The Art of Software Testing',
+    description: 'This book provides a practical rather than theoretical discussion of the purpose and nature of software testing.',
+    numberOfPages: 192,
+    author: 'Glenford J. Myers',
+    reading: false
+    
+};
+
+console.log(books.title)
+console.log(books.description)
+console.log(books.numberOfPages)
+console.log(books.author)
+console.log(books.reading)
