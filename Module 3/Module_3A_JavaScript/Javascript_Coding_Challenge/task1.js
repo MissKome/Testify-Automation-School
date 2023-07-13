eb4a74e22c1108ef03b3b91fcfac75397aa6ad9d
Oghenekome Edaware <edawarekome@gmail.com>
2023-07-13 17:09:36 +0100

    Calculate the sum of numbers within an array.

--- Module 3/Module_3A_JavaScript/Javascript_Coding_Challenge/task1.js
+++ Module 3/Module_3A_JavaScript/Javascript_Coding_Challenge/task1.js
@@ -1,9 +1,13 @@
 //Calculate the sum of numbers within an array.
+
 const numbers = [1, 21, 3, 74, 15];
+
 let sum = 0;
 
 numbers.forEach(num => {
+
     sum += num;
+    
 });
 
 console.log(sum);
