d2c45f7d0f65ba241d0d85a2ae24c20117f13c5a
Oghenekome Edaware <edawarekome@gmail.com>
2023-07-13 18:17:50 +0100

    Print a table containing multiplication tables.

---
+++ Module 3/Module_3A_JavaScript/Javascript_Coding_Challenge/task4.js
@@ -0,0 +1,12 @@
+//Print a table containing multiplication tables.
+
+const number = 6
+
+for(let i = 1; i <= 12; i++) {
+
+    const result = i * number;
+    
+    console.log(number + ' * ' + i + ' = ' + result);
+
+}
+
