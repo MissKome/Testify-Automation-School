66cd7213b80f8ad853572de934b0d2d36dcc3c26
Oghenekome Edaware <edawarekome@gmail.com>
2023-07-13 19:50:06 +0100

    Sort an array of strings in alphabetical order.

---
+++ Module 3/Module_3A_JavaScript/Javascript_Coding_Challenge/task6.js
@@ -0,0 +1,7 @@
+//Sort an array of strings in alphabetical order.
+
+const food = ['Yam', 'Rice', 'Potatoe', 'Plantain', 'Salad'];
+
+sortFood = food.sort();  //sorts food in alphabetical order
+
+console.log (sortFood);
