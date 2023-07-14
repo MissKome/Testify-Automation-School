1cc529ae44997d31642306f3a0f43aafaf0e7001
Oghenekome Edaware <edawarekome@gmail.com>
2023-07-14 10:01:10 +0100

    Create a length converter function

--- Module 3/Module_3A_JavaScript/Javascript_Coding_Challenge/task2.js
+++ Module 3/Module_3A_JavaScript/Javascript_Coding_Challenge/task2.js
@@ -1,1 +1,42 @@
 //Create a length converter function
+
+function lengthConverter(length, fromUnit, toUnit) {
+    
+    const lengthConversions = {
+      millimeter: {
+        centimeter: 0.1,
+        meter: 0.001,
+        kilometer: 0.000001,
+        
+      },
+      centimeter: {
+        millimeter: 10,
+        meter: 0.01,
+        kilometer: 0.00001,
+       
+      },
+      meter: {
+        millimeter: 1000,
+        centimeter: 100,
+        kilometer: 0.001,
+       
+      },
+      kilometer: {
+        millimeter: 1000000,
+        centimeter: 100000,
+        meter: 1000,
+      
+      },
+    
+    };
+  
+    if (!lengthConversions[fromUnit] || !lengthConversions[fromUnit][toUnit]) { 
+      throw new Error('Invalid length units');
+    }
+  
+    const conversion = lengthConversions[fromUnit][toUnit]; //converts
+    const convertedLength = length * conversion;   //converted length
+    return convertedLength;
+  }
+
+  console.log(lengthConverter(500, 'meter', 'centimeter')); 
