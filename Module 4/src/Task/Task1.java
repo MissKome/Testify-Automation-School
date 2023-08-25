f993f80d8579e6773a5756d5817c856defee0288
Oghenekome Edaware <edawarekome@gmail.com>
2023-08-25 09:28:20 +0100

    Print out several lines on your console

--- Module 4/src/Task/Task1.java
+++ Module 4/src/Task/Task1.java
@@ -1,4 +1,11 @@
 package Task;
 
 public class Task1 {
+    public static void main(String[] args) {
+
+        System.out.println("Hello world!");
+        System.out.println("I just started my java class.");
+        System.out.println("This is my first task.");
+        System.out.println("It is Task1");
+    }
 }
