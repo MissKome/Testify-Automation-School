3bc2a84c3a6bc304d5ac6bb783e176f30bf6b54e
Oghenekome Edaware <edawarekome@gmail.com>
2023-08-25 09:53:23 +0100

    Print out all 6 variables that are all different data types on your console

---
+++ Module 4/src/Task/Task2.java
@@ -0,0 +1,25 @@
+package Task;
+
+public class Task2 {
+    public static void main(String[] args) {
+        byte cars = 10;
+
+        short books = 15_000;
+
+        int cashMoney = 2_000_000;
+
+        boolean myMoney = true;
+
+        char option = 'b';
+
+        float area = 132536.11f;
+
+        System.out.println(cars);
+        System.out.println(books);
+        System.out.println(cashMoney);
+        System.out.println(myMoney);
+        System.out.println(option);
+        System.out.println(area);
+
+    }
+}
