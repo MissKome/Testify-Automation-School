50f6553396982c7a9811e1f80f3f6bcd905fff91
Oghenekome Edaware <edawarekome@gmail.com>
2023-08-25 11:58:38 +0100

    Find the area of a circle with radius on12.7m

---
+++ Module 4/src/Task/Task4.java
@@ -0,0 +1,13 @@
+package Task;
+
+public class Task4 {
+    public static void main(String[] args) {
+        //Area of a circle
+        double radius = 12.7; //radius in meters
+        double pi = 3.14;  //π
+        double area = pi * radius * radius; //area of a circle is A=πr2
+
+        // Print the result
+        System.out.println("The area of the circle with radius " + radius + "m is " + area + " meters.");
+    }
+}
