a80ebd61b8a6207df2ba44a972e013ab60c52185
Oghenekome Edaware <edawarekome@gmail.com>
2023-08-30 11:30:49 +0100

    Create a class A, create a method that accepts a String parameter "name" and returns the "name" value. Create an object of that class in another class  "B" and SOUT the contents from the methods in class A

---
+++ Module 4/src/Task/Task11/Class_A.java
@@ -0,0 +1,13 @@
+package Task.Task11;
+
+public class Class_A {
+
+    String name;
+    public void isMyName(String myName){
+        name = myName;
+
+        System.out.println("My name is " + name + "." );
+    }
+
+
+}
---
+++ Module 4/src/Task/Task11/Class_B.java
@@ -0,0 +1,13 @@
+package Task.Task11;
+
+public class Class_B {
+    public static void main(String[] args) {
+        Class_A nameTask = new Class_A();
+       nameTask.name = "Mary";
+
+
+       nameTask.isMyName(nameTask.name);
+
+    }
+
+}
