5fba7edd09369c5eaf3f571da2c4cfd2379f8fe7
Oghenekome Edaware <edawarekome@gmail.com>
2023-09-03 21:20:39 +0100

    Create 3 different constructors of a class A. Create 3 objects of Class A in class B and use the 3 different constructors when creating each of the objects of class A in class B

---
+++ Module 4/src/Task/Task13/ClassA.java
@@ -0,0 +1,29 @@
+package Task.Task13;
+
+public class ClassA {
+
+    String food;
+    int types;
+    boolean source;
+
+    public ClassA(String foodType) {
+        food = foodType;
+    }
+
+    public ClassA(String secondFood, int myTypes) {
+        food = secondFood;
+        types = myTypes;
+    }
+
+    public ClassA(String thirdFood, int thisTypes, boolean isSource) {
+        food = thirdFood;
+        types = thisTypes;
+        source = isSource;
+    }
+
+
+    }
+
+
+
+
---
+++ Module 4/src/Task/Task13/ClassB.java
@@ -0,0 +1,12 @@
+package Task.Task13;
+
+public class ClassB {
+    public static void main(String[] args) {
+        ClassA myFood = new ClassA("rice");
+        ClassA myTypes = new ClassA("beans", 5);
+        ClassA mySource = new  ClassA("yam and fish", 7, true);
+
+//
+    }
+}
+
