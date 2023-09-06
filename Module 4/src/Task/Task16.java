c84f0d01f02c6a7c517581f841fc3749b82c1100
Oghenekome Edaware <edawarekome@gmail.com>
2023-09-06 21:19:17 +0100

    OVERLOADING: Create a method in a class and Overload in using the 3 different ways of overloading that you know.

---
+++ Module 4/src/Task/Task16.java
@@ -0,0 +1,36 @@
+package Task;
+
+public class Task16 {
+
+    //method
+    public void foodTypes (){
+
+    }
+    //difference in the number of parameters
+    public void  foodTypes (String rice){
+
+    }
+
+    public void  foodTypes (String carbohydrate, String protein){
+
+    }
+//difference in the data type
+
+    public void foodTypes (boolean condition, int number){
+
+    }
+
+    public void foodTypes (int fruit, int number){
+
+    }
+
+    // difference in the data type sequence or position
+
+    public void foodTypes (String fruit, int number){
+
+    }
+
+    public void foodTypes ( int number, String fruit){
+
+    }
+}
