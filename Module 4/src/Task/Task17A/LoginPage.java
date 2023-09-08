c1ba91d8d52abdc7285afa9aef297aa32e995140
Oghenekome Edaware <edawarekome@gmail.com>
2023-09-08 13:12:20 +0100

    You are required to build the blueprint for your company's login page. From the discussions with the product team, all login pages will have a username field, password field, forgot password field, and sign-in button while other elements like remember me checkbox, continue to homePage, OAuth button will not be applicable to all login pages. with your knowledge of Abstract classes, Create a class that others developers will have to inherit from.

---
+++ Module 4/src/Task/Task17A/LoginPage.java
@@ -0,0 +1,22 @@
+package Task.Task17A;
+
+public abstract class LoginPage {
+
+    public void enterUsername(String username){
+
+    }
+
+    public void enterPassword(String password){
+
+    }
+
+    public void forgotPassword(){
+
+    }
+
+    public void signIn(){
+
+    }
+
+    public abstract void getOAuth();
+}
