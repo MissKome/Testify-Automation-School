31bd24e7159c76a5a1fc22a8587e5256db6d9f26
Oghenekome Edaware <edawarekome@gmail.com>
2023-09-11 10:33:33 +0100

    STATIC: I want to use the value of a variable in one class in another class at runtime. I also want to access the variable without creating an object of its class. Help me implement this

---
+++ Module 4/src/Task/Task19b/OneClass.java
@@ -0,0 +1,6 @@
+package Task.Task19b;
+
+public class OneClass {
+    static String ballColor = "pink";
+
+}
---
+++ Module 4/src/Task/Task19b/SecondClass.java
@@ -0,0 +1,7 @@
+package Task.Task19b;
+
+public class SecondClass {
+    public static void main(String[] args) {
+        System.out.println(OneClass.ballColor);
+    }
+}
