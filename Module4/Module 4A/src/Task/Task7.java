995d28bf874e08c9938d1b960474dffa4c8b0643
Oghenekome Edaware <edawarekome@gmail.com>
2023-09-15 14:22:41 +0100

    create a two dimensional array with 4 rows and 3 columns. Fill only the first column with any fruits of ur choice

--- Module4/Module 4A/src/Task/Task7.java
+++ Module4/Module 4A/src/Task/Task7.java
@@ -1,10 +1,11 @@
 package Task;
-
+//create a two dimensional array with 4 rows and 3 columns. Fill only the first column with any fruits of ur choice
 import java.util.Arrays;
 
 public class Task7 {
     public static void main(String[] args) {
         String [] [] doubleDimension = new String[4][3];
+
         //filling the first column with fruits
         doubleDimension[0] [0] = "Apple";
         doubleDimension[1] [0] = "Banana";
