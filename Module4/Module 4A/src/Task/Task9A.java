a852abdebebde0c91f722e6428d2c710b98c15b7
Oghenekome Edaware <edawarekome@gmail.com>
2023-09-15 14:26:06 +0100

    I have a list of numbers, 1-10, write a loop that prints odd if it is an odd number and a loop that prints even if it is an even number.

--- Module4/Module 4A/src/Task/Task9A.java
+++ Module4/Module 4A/src/Task/Task9A.java
@@ -1,5 +1,5 @@
 package Task;
-
+//I have a list of numbers, 1-10, write a loop that prints odd if it is an odd number and a loop that prints even if it is an even number.
 public class Task9A { //a loop that prints odd if it is an odd number and a loop that prints even if it is an even number.
 
     public static void main(String[] args) {
