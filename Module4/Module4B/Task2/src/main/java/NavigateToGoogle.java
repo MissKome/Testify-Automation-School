d728428d436d69483cf842eb3d614daf23e7e65c
Oghenekome Edaware <edawarekome@gmail.com>
2023-09-19 18:42:36 +0100

    Create three different classes and launch any three websites of your choice from each of them

---
+++ Module4/Module4B/Task2/src/main/java/NavigateToGoogle.java
@@ -0,0 +1,12 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class NavigateToGoogle {
+    public static void main(String[] args) {
+        System.setProperty("webdriver.chrome.driver", "C:\\Users\\Windows 10 Pro\\IdeaProjects\\Testify-Automation-School\\Module4\\Module4B\\Task2\\src\\chromedriver-win64\\chromedriver-win64\\chromedriver.exe");
+
+        WebDriver driver = new ChromeDriver();
+
+        driver.get("https://www.google.com/");
+    }
+}
