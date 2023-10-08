d79137d79aaffa9cbaaba311c1f80b3831ec8e0e
Oghenekome Edaware <edawarekome@gmail.com>
2023-10-08 18:45:24 +0100

    Using location strategy

--- Module4/Module_4B/src/main/java/Task4.java
+++ Module4/Module_4B/src/main/java/Task4.java
@@ -5,7 +5,7 @@ import org.openqa.selenium.chrome.ChromeDriver;
 
 public class Task4 {
     public static void main(String[] args) throws InterruptedException {
-        System.setProperty("webdriver.chrome.driver", "C:\\Users\\Windows 10 Pro\\IdeaProjects\\Testify-Automation-School\\Module4\\Module4B\\Task4\\src\\chromedriver-win64\\chromedriver-win64\\chromedriver.exe");
+        System.setProperty("webdriver.chrome.driver", "C:\\Users\\Windows 10 Pro\\IdeaProjects\\Testify-Automation-School\\Module4\\Module_4B\\src\\chromedriver-win64\\chromedriver-win64\\chromedriver.exe");
 
         WebDriver driver = new ChromeDriver();
 
